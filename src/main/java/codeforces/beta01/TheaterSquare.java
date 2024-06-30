7aa414e7aae0aa2e287105047700bfc48b36a572
ackasaber <ackasaber@gmail.com>
2024-06-30 11:43:37 +0300

    Cleaned code

--- src/main/java/codeforces/beta01/TheaterSquare.java
+++ src/main/java/codeforces/beta01/TheaterSquare.java
@@ -1,41 +1,33 @@
 package codeforces.beta01;
 
-/* Task 1A. Theater square. */
+/* A. Theater square. */
 
 import java.util.Scanner;
 import static java.lang.System.out;
-
-import java.nio.charset.StandardCharsets;
+import static java.nio.charset.StandardCharsets.US_ASCII;
 
 public class TheaterSquare {
 	static long flagstoneCount(int n, int m, int a) {
-		/* multiplication in the end would require a wider type */
-		long N = n;
-		long M = m;
-		long A = a;
-		/* ceil(N/A) */
-		long oneside = (N + A - 1) / A;
-		/* ceil(M/A) */
-		long otherside = (M + A - 1) / A;
-		return oneside * otherside;
+		if (n < 1 || n > 1000_000_000)
+			throw new IllegalArgumentException("n out of range");
+		if (m < 1 || m > 1000_000_000)
+			throw new IllegalArgumentException("m out of range");
+		if (a < 1 || a > 1000_000_000)
+			throw new IllegalArgumentException("a out of range");
+        // ceil(n/a)
+		int oneSide = (n + a - 1) / a;
+		// ceil(m/a)
+		int otherSide = (m + a - 1) / a;
+		// Result might not fit into int, so cast the first operand.
+		return (long) oneSide * otherSide;
 	}
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in, StandardCharsets.US_ASCII);
+		Scanner scanner = new Scanner(System.in, US_ASCII);
 		int n = scanner.nextInt();
 		int m = scanner.nextInt();
 		int a = scanner.nextInt();
-
-		if (n < 1 || n > 1000000000)
-			out.println("WRONG n");
-		else if (m < 1 || m > 1000000000)
-			out.println("WRONG m");
-		else if (a < 1 || a > 1000000000)
-			out.println("WRONG a");
-		else {
-			long count = flagstoneCount(n, m, a);
-			out.println(count);
-		}
+		long count = flagstoneCount(n, m, a);
+		out.println(count);
 	}
 }
