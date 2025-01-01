fb723185c256612c75eff94d7d9236b0b4616865
ackasaber <ackasaber@gmail.com>
2025-01-01 20:08:03 +0300

    A surprising O(1) space solution.

--- bazel/trapping_rainwater/trapping_rainwater.cc
+++ bazel/trapping_rainwater/trapping_rainwater.cc
@@ -1,34 +1,32 @@
+#include <memory>
+#include <algorithm>
+
 #include "trapping_rainwater.h"
 
 using absl::Span;
 
-int max_in(Span<const int> a, int initial);
-int min_of(int a, int b);
-
 int trap_rainwater(Span<const int> heights) {
-  int volume = 0;
   int n = heights.size();
+  int max_left = 0;
+  int max_right = 0;
+  int volume = 0;
+  int left = 0;
+  int right = n - 1;
+  // Invariant: heights[0..left-1] and heights[right+1..n-1] are accounted.
 
-  for (int i = 0; i < n; i++) {
-    int h1 = max_in(heights.first(i), 0);
-    int h2 = max_in(heights.last(n - i - 1), 0);
-    int level = min_of(h1, h2);
-    int h = heights[i];
-
-    if (level > h) volume += level - h;
+  while (left <= right) {
+    if (max_left > max_right) {
+      // heights[right+1] cannot be a global maximum
+      max_right = std::max(max_right, heights[right]);
+      volume += max_right - heights[right];
+      right--;
+    } else {
+      // heights[left] cannot be a single global maximum
+      max_left = std::max(max_left, heights[left]);
+      volume += max_left - heights[left];
+      left++;
+    }
   }
 
   return volume;
 }
-
-int max_in(Span<const int> a, int initial) {
-  int max = initial;
-
-  for (int x : a) {
-    if (x > max) max = x;
-  }
-
-  return max;
-}
-
-int min_of(int a, int b) { return a < b ? a : b; }
